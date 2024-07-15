2d840604d846d3221d4f473458b9f9d5b339f3d0
pppyyyccc <panyongcun@gmail.com>
2024-07-15 14:16:50 +0800

    Update code to version 0.5

--- index.js
+++ index.js
@@ -5,51 +5,94 @@ const { Headers, Request } = require('node-fetch');
 
 // 解析环境变量
 const CUSTOM_DOMAIN = process.env.CUSTOM_DOMAIN || 'default-domain.com';
+const SECURITY_TOKEN = process.env.SECURITY_TOKEN || '';
 const parsedUrl = url.parse(CUSTOM_DOMAIN);
 const protocol = parsedUrl.protocol || (CUSTOM_DOMAIN.startsWith('https') ? 'https:' : 'http:');
 const host = parsedUrl.hostname || CUSTOM_DOMAIN.split('/')[2].split(':')[0];
 const port = parsedUrl.port || (protocol === 'https:' ? '443' : '80');
 
 // 生成URL
-const YSP_URL = `${protocol}//${host}${port ? `:${port}` : ''}/ysp.m3u`;
 const FOUR_SEASONS_URL = `${protocol}//${host}${port ? `:${port}` : ''}/4gtv.m3u`;
+const BEESPORT_URL = `${protocol}//${host}${port ? `:${port}` : ''}/beesport.m3u`;
+const YSP_URL = `${protocol}//${host}${port ? `:${port}` : ''}/ysp.m3u`;
+const SXG_URL = `${protocol}//${host}${port ? `:${port}` : ''}/sxg.m3u`;
+const ITV_PROXY_URL = `${protocol}//${host}${port ? `:${port}` : ''}/itv_proxy.m3u`;
+const TPTV_PROXY_URL = `${protocol}//${host}${port ? `:${port}` : ''}/tptv_proxy.m3u`;
+const MYTVSUPER_URL = `${protocol}//${host}${port ? `:${port}` : ''}/mytvsuper.m3u`;
 const PROXY_DOMAIN = host;
 
 // 定义源地址
 const SRC = [
   {
+    name: '四季',
+    url: FOUR_SEASONS_URL,
+    mod: (noproxy) => noproxy ? identity : proxify
+  },
+  {
+    name: 'MytvSuper 直播源',
+    url: MYTVSUPER_URL,
+    mod: (noproxy) => noproxy ? identity : proxify
+  },
+  {
+    name: 'Beesport 直播源',
+    url: BEESPORT_URL
+  },
+  {
     name: '央视频',
     url: YSP_URL
   },
   {
-    name: '四季',
-    url: FOUR_SEASONS_URL,
-    mod: (noproxy) => noproxy ? identity : proxify
+    name: '蜀小果',
+    url: SXG_URL
+  },
+  {
+    name: '中国移动 iTV 平台',
+    url: ITV_PROXY_URL
+  },
+  {
+    name: '江苏移动魔百盒 TPTV',
+    url: TPTV_PROXY_URL
   }
 ];
 
 // 定义代理域名
 const PROXY_DOMAINS = [
   PROXY_DOMAIN,
-  '[^/]+\\.hinet\\.net',
-  '[^/]+\\.googlevideo\\.com',
-  '[^/]+\\.tvb.com(:\\d+)?'
+  '[^/]+\\.hinet\\.net(:\\d+)?',
+  '[^/]+\\.googlevideo\\.com(:\\d+)?',
+  '[^/]+\\.tvb.com(:\\d+)?',
+  '[^/]+\\.livednow\\.com(:\\d+)?',
+  '[^/]+\\.orz-7\\.com(:\\d+)?',
+  '[^/]+\\.4gtv\\.tv(:\\d+)?',
+  '[^/]+\\.ofiii\\.com(:\\d+)?',
+  '[^/]+\\.youtube\\.com(:\\d+)?',
+  '[^/]+\\.mytvsuper\\.com(:\\d+)?',
+  '[^/]+\\.beesport\\.livednow\\.com(:\\d+)?'
 ];
 
 function identity(it) { return it; }
 
 function proxify(it) {
   for (const dom of PROXY_DOMAINS) {
-    it = it.replace(new RegExp('https?://' + dom, 'g'), process.env.VPS_HOST + '/proxy/$&');
+    it = it.replace(new RegExp('https?://' + dom, 'g'), `${process.env.VPS_HOST}/${SECURITY_TOKEN}/proxy/$&`);
   }
   return it;
 }
 
 const server = http.createServer(async (req, res) => {
-  if (req.url.startsWith('/proxy')) {
-    await handleProxy(req, res);
+  const token = req.url.split('/')[1];
+  if (token === SECURITY_TOKEN) {
+    if (req.url.startsWith(`/${SECURITY_TOKEN}/proxy`)) {
+      await handleProxy(req, res);
+    } else if (req.url.startsWith(`/${SECURITY_TOKEN}`)) {
+      await handleList(req, res);
+    } else {
+      res.writeHead(403, { 'Content-Type': 'text/plain' });
+      res.end('Forbidden');
+    }
   } else {
-    await handleList(req, res);
+    res.writeHead(403, { 'Content-Type': 'text/plain' });
+    res.end('Forbidden');
   }
 });
 
@@ -88,7 +131,7 @@ async function handleList(req, res) {
 
 async function handleProxy(req, res) {
   try {
-    const targetUrl = new URL(req.url.split('/proxy/')[1]);
+    const targetUrl = new URL(req.url.split(`/${SECURITY_TOKEN}/proxy/`)[1]);
     console.log('proxying', targetUrl);
 
     const resp = await fetch(targetUrl);
